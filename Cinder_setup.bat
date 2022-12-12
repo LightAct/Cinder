98eda25a20e621724e95701eaeb5eb59ef1b85c7
daviddev <david.zalik@gmail.com>
2022-12-12 12:27:51 +0100

    fixed cinder bat

--- Cinder_setup.bat
+++ Cinder_setup.bat
@@ -1,5 +1,12 @@
 @ECHO OFF
 
+rem -- 7zip checker --
+if not exist "C:\Program Files\7-Zip\7z.exe" (
+    echo 7zip is required to run this batch file. Install 7zip.
+    pause
+    exit
+)
+
 ECHO Downloading files...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://lightact.io/uploads/swDevRepository/cinder-blocks.zip', 'cinder-blocks.zip')"
 
